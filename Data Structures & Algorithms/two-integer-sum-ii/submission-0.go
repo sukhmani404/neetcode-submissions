ac37f48bbaed383d122d3c7c71f5005533e57318
Sukhmani Singh <48706580+sukhmani404@users.noreply.github.com>
2026-05-20 16:08:25 +0530

    Add: two-integer-sum-ii - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-0.go
@@ -0,0 +1,16 @@
+func twoSum(numbers []int, target int) []int {
+	i, j := 0, len(numbers)-1
+
+	for i < j {
+		sum := numbers[i] + numbers[j]
+		if sum == target {
+			return []int{i+1, j+1}
+		}else if sum > target {
+			j--
+		}else{
+			i++
+		}
+	}
+
+	return nil //impossible condition?
+}
