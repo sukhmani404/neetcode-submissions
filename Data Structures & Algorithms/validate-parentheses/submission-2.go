cf5353d4ceee0e93d8104278f2429f15a5b2d6f4
Sukhmani Singh <48706580+sukhmani404@users.noreply.github.com>
2026-05-27 11:46:11 +0530

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.go
@@ -0,0 +1,36 @@
+func isValid(s string) bool {
+	if len(s)%2 != 0 {
+		return false //if not pairs
+	}
+	
+    opp := map[byte]byte{
+		'}': '{',
+		')': '(',
+		']': '[',
+	}
+
+	stack := []byte{}
+
+	for i := 0; i < len(s); i++ {
+		if isOpeningBracket(s[i]) {
+			stack = append(stack, s[i])
+		}else if isClosingBracket(s[i]) {
+			if len(stack) == 0 || opp[s[i]] != stack[len(stack)-1] {
+				return false
+			}
+
+			stack = stack[:len(stack)-1]
+		}	
+		
+	}
+
+	return len(stack) == 0
+}
+
+func isOpeningBracket(b byte) bool {
+	return b == '(' || b == '{' || b == '['
+}
+
+func isClosingBracket(b byte) bool {
+	return b == ')' || b == '}' || b == ']'
+}
