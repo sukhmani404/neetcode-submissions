1dc2009106bd98dcf76797d4fcbe05dd13d81dee
Sukhmani Singh <48706580+sukhmani404@users.noreply.github.com>
2026-06-08 07:21:53 +0530

    Add: duplicate-integer - submission-2

---
+++ Data Structures & Algorithms/duplicate-integer/submission-2.go
@@ -0,0 +1,13 @@
+func hasDuplicate(nums []int) bool {
+    seen := map[int]bool{}
+
+    for _, num := range nums {
+        if _, ok := seen[num]; ok {
+            return true
+        }
+
+        seen[num] = true
+    }
+
+    return false
+}
