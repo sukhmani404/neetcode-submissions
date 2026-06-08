057494497452c67c6d39f0258d0fa25489a2e139
Sukhmani Singh <48706580+sukhmani404@users.noreply.github.com>
2026-06-08 07:51:56 +0530

    Add: top-k-elements-in-list - submission-6

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-6.go
@@ -0,0 +1,28 @@
+func topKFrequent(nums []int, k int) []int {
+	freq := map[int]int{}
+	maxFreq := 0
+
+	for _, num := range nums {
+		freq[num]++
+		maxFreq = max(maxFreq, freq[num])
+	}
+
+	buckets := make([][]int, maxFreq+1)
+
+	for c, f := range freq {
+		buckets[f] = append(buckets[f], c)
+	}
+
+	ans := []int{}
+
+	for f := maxFreq; f >=0; f-- {
+		ans = append(ans, buckets[f]...)
+
+		if len(ans) >= k {
+			ans = ans[:k]
+			break
+		}
+	}
+
+	return ans
+}
